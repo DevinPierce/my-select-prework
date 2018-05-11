a46fa36592893bf490082acaf2c959a8704ab0d9
Devin Pierce <devinmpierce@gmail.com>
2018-05-11 14:29:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
   new_collection = []
   i = 0
   while (i < collection.length) do
-    if (yield(arr[i]))
-      new_collection << arr[i]
+    if (yield(collection[i]))
+      new_collection << collection[i]
     end
     i+=1
   end
